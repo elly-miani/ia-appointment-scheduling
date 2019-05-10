cfdf845b3336ccb0ad78122df8fb25ac48ffae33
Eleonora Miani <miani.eleonora@gmail.com>
2019-05-10 11:12:45 +0200

    Refactored py.constraint Solver

--- Solver/Solver.py
+++ Solver/Solver.py
@@ -1,21 +1,30 @@
-from constraint import *
+from constraint import Problem
+import pprint as pp
 import random
 import csv
 import time
 import sys
 
 
-
 current_milli_time = lambda: int(round(time.time() * 1000))
-
 
 
 def constraintFunction():
     def innerFunction (x, y):
+        # [0] is 'Day': if they are in different days there's no needed constraint 
         if x[0] != y[0]:
             return True
-        # se sono qui sono sicuro che sto considerando due appuntamenti che avvengono nello stesso giorno.
-        #print("x = ", x, "   y = ", y)
+
+        # else: they are the same day
+        # [1] is 'Hour', [2] is 'House'
+        # if the houses are different, the distance 
+        # print(x[1])
+        # print(y[1])
+        # print(float(x[1])-float(y[1]))
+        # print(abs(float(x[1])-float(y[1])))
+        # print(distance(x[2], y[2]))
+        # print(distance(x[2], y[2])*0.5 + 1)
+        
         if (x[2] != y[2] and abs(float(x[1])-float(y[1])) < distance(x[2], y[2])*0.5 + 1):
             return False
         if (x[2] == y[2] and abs(float(x[1])-float(y[1])) != 1):
@@ -24,9 +33,6 @@ def constraintFunction():
             return True
     return innerFunction
 
-
-def takeSecond(elem):
-    return elem[1]
 
 def distance(a, b):
     if (a=='A' and b=='B') or (b=='A' and a=='B'):
@@ -45,148 +51,136 @@ def distance(a, b):
         return 0
 
 
+def takeSecond(elem):
+    return elem[1]
 
 
-appointments = dict()
 
-"""
-appointment1 = {
-  "Name": "Ford",
-  "Surname": "Mustang",
-  "House" : "A",
-  "Day" : ["mon", "wed", "fri"],
-  "Day" : ["mon"],
-  "Pref" : ["Morning"]
-}
+# intialize a generic domain with all possible combinations of days, hours and locations
+def initDomain():
+    days = ["mon", "tue", "wed", "thu", "fri", "sat"]
 
-appointment2 = {
-  "Name": "Ford",
-  "Surname": "Mustang",
-  "House" : "A",
-  "Day" : ["mon"],
-  "Pref" : ["Morning"]
-}
+    hours = ["08.00", "08.50", "09.00", "09.50", "10.00", "10.50", "11.00", "11.50",
+            "13.00", "13.50", "14.00", "14.50", "15.00", "15.50", "16.00", "16.50", "17.00",
+            "17.50"]
+
+    locations = ["A", "B", "C", "D"]
+
+    #prefs = ["Morning", "Afternoon"]
 
 
-appointments["1"] = appointment1
-appointments["2"] = appointment2
-"""
+    dominio = []
+    count = 0
 
-days = ["mon", "tue", "wed", "thu", "fri", "sat"]
+    for i in days:
+        for y in hours:
+            for loc in locations:
+                dominio.append([i])
+                dominio[count].append(y)
+                dominio[count].append(loc)
+                count += 1
 
-hours = ["08.00", "08.50", "09.00", "09.50", "10.00", "10.50", "11.00", "11.50",
-"13.00", "13.50", "14.00", "14.50", "15.00", "15.50", "16.00", "16.50", "17.00",
-"17.50"]
-
-locations = ["A", "B", "C", "D"]
-
-surnames = [ "Smith", "Jones", "Taylor", "Williams", "Brown", "Davies", "Evans",
-"Wilson", "Thomas", "Roberts", "Johnson", "Lewis", "Walker", "Robinson", "Wood",
-"Thompson", "White", "Watson", "Jackson", "Wright", "Green", "Harris", "Cooper",
-"King", "Lee", "Martin", "Clarke", "James", "Morgan", "Hughes"]
-
-names = ["Liam", "Emma", "Noah", "Olivia", "William", "Ava", "James",
-"Isabella", "Logan", "Sophia", "Benjamin", "Mia", "Mason", "Charlotte",
-"Elijah", "Amelia", "Oliver", "Evelyn", "Jacob", "Abigail", "Lucas", "Harper",
-"Michael", "Emily", "Alexander", "Elizabeth", "Ethan", "Avery", "Daniel",
-"Sofia"]
-
-prefs = ["Morning", "Afternoon"]
-"""
-for i in range(30):
-    appointment = {
-      "Name": names[random.randint(0, len(names)-1)],
-      "Surname": surnames[random.randint(0, len(surnames)-1)],
-      "House" : locations[random.randint(0, len(locations)-1)],
-      "Day" : [days[random.randint(0, len(days)-1)], days[random.randint(0, len(days)-1)], days[random.randint(0, len(days)-1)]],
-      "Pref" : [prefs[random.randint(0, len(prefs)-1)]]
-    }
-    appointments[str(i)] = appointment
-"""
-appointments = {}
-with open(sys.argv[1], 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        a = iter(row[1:])
-        appointments[row[0]] = dict(zip(a, a))
-print(appointments)
+    return dominio
 
 
-dominio = []
-count = 0
+# load appointments from a csv file in *filePath*
+def loadAppointments(filePath):
+    appointments = {}
+
+    with open(filePath, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            a = iter(row[1:])
+            appointments[row[0]] = dict(zip(a, a))
+
+    return appointments
 
 
-for i in days:
-    for y in hours:
-        for loc in locations:
-            dominio.append([i])
-            dominio[count].append(y)
-            dominio[count].append(loc)
-            count += 1
+def solver(domain, appointments):
+    problem = Problem()
 
-#print(dominio)
-#print(appointments)
+    # for each appointment (iterate on the numerical key of the appointments)
+    for x in appointments:
+        dom = []
 
-problem = Problem()
+        # check which elements of the generic domain are necessary for this appointment
+        for y in domain:
+            hour, minutes = y[1].split(".")
+            hour = int(hour)
 
-# sto iterando su tutte le chiavi
-for x in appointments:
-    dom = []
+            if "Morning" in appointments[x]["Pref"] and hour < 12 and y[0] in appointments[x]["Day"] and y[2] in appointments[x]["House"]:
+                    dom.append(y)
 
-    for y in dominio:
-        hour , minutes = y[1].split(".")
-        hour = int(hour)
+            if "Afternoon" in appointments[x]["Pref"] and hour > 12 and y[0] in appointments[x]["Day"] and y[2] in appointments[x]["House"]:
+                    dom.append(y)
+        
+        # print("FOR APPOINTMENT:\n")
+        # pp.pprint(x)
+        # print("The domain is:\n")
+        # pp.pprint(dom)
 
-        if "Morning" in appointments[x]["Pref"] and hour < 12 and y[0] in appointments[x]["Day"] and y[2] in appointments[x]["House"]:
-                dom.append(y)
+        # add a variable to the CSP problem with the appointment *x* and the just computed domain *dom*
+        problem.addVariable(x, dom)
+    
+    # add constraints
+    for x in appointments:
+        for y in appointments:
+            if(x != y):
+                problem.addConstraint(constraintFunction(), (x, y))
 
-        if "Afternoon" in appointments[x]["Pref"] and hour > 12 and y[0] in appointments[x]["Day"] and y[2] in appointments[x]["House"]:
-                dom.append(y)
+    start = current_milli_time()
+    solution = problem.getSolution()
+    end = current_milli_time()
+    print("\n\n###########Time spent to find the first solution = ", end-start, " ms.\n\n")
+    
+    # pp.pprint(solution)
 
-    #print(dom)
-    problem.addVariable(x, dom)
+    return solution
 
 
-for x in appointments:
-    for y in appointments:
-        if(x != y):
-            #print("Aggiungo un constraint")
-            problem.addConstraint(constraintFunction(), (x, y))
+# print function to print a solution in a clean way
+def printSolution(solution):
+    ordApp = [[], [], [], [], [], []]
 
-start = current_milli_time()
-solution = problem.getSolution()
-end = current_milli_time()
-print("\n\n###########Time spent to find the first solution = ", end-start," ms.\n\n")
-print(solution)
+    days = ["mon", "tue", "wed", "thu", "fri", "sat"]
 
-ordApp = [[],[],[],[],[],[]]
+    for x in solution:
+        if solution[x][0] == days[0]:
+            ordApp[0].append([x, solution[x]])
+        if solution[x][0] == days[1]:
+            ordApp[1].append([x, solution[x]])
+        if solution[x][0] == days[2]:
+            ordApp[2].append([x, solution[x]])
+        if solution[x][0] == days[3]:
+            ordApp[3].append([x, solution[x]])
+        if solution[x][0] == days[4]:
+            ordApp[4].append([x, solution[x]])
+        if solution[x][0] == days[5]:
+            ordApp[5].append([x, solution[x]])
 
-for x in solution:
-    if solution[x][0]==days[0]:
-        ordApp[0].append([x, solution[x]])
-    if solution[x][0]==days[1]:
-        ordApp[1].append([x, solution[x]])
-    if solution[x][0]==days[2]:
-        ordApp[2].append([x, solution[x]])
-    if solution[x][0]==days[3]:
-        ordApp[3].append([x, solution[x]])
-    if solution[x][0]==days[4]:
-        ordApp[4].append([x, solution[x]])
-    if solution[x][0]==days[5]:
-        ordApp[5].append([x, solution[x]])
+    for x in ordApp:
+        x.sort(key=takeSecond)
 
-print(ordApp)
-for x in ordApp:
-    x.sort(key =  takeSecond)
+    index = 0
+    for x in ordApp:
+        print("\n\nGiorno: ", days[index])
+        print("\nMattina:")
+        cond = True
+        for y in x:
+            if (cond and float(y[1][1]) > 12):
+                print("\nPomeriggio:")
+                cond = False
+            print("Ore: ", y[1][1], "Casa: ", y[1][2], " Appuntamento con: ",
+                  appointments[y[0]]["Name"], " ", appointments[y[0]]["Surname"])
+        index += 1
 
-index = 0
-for x in ordApp:
-    print("\n\nGiorno: ", days[index])
-    print("\nMattina:")
-    cond = True
-    for y in x:
-        if (cond and float(y[1][1])>12):
-            print("\nPomeriggio:")
-            cond = False
-        print("Ore: ", y[1][1], "Casa: ", y[1][2], " Appuntamento con: ", appointments[y[0]]["Name"], " ", appointments[y[0]]["Surname"])
-    index+=1
+
+
+
+
+# main 
+domain = initDomain()
+appointments = loadAppointments(sys.argv[1])
+
+solution = solver(domain, appointments) 
+printSolution(solution)
--- docs.md
+++ docs.md
@@ -7,3 +7,44 @@
 
 
 La data di inizio del job 3 >= 5 gg + inizio job 1
+
+# Questions
+
+## `Solver.py`
+
+- Why are some hours at 50 minutes?
+
+- Why does this `constraintFunction()` not have parameters?
+
+```
+def constraintFunction():
+    # for each 
+    def innerFunction (x, y): 
+        # if 
+        if x[0] != y[0]:
+            return True
+        # se sono qui sono sicuro che sto considerando due appuntamenti che avvengono nello stesso giorno.
+        #print("x = ", x, "   y = ", y)
+        if (x[2] != y[2] and abs(float(x[1])-float(y[1])) < distance(x[2], y[2])*0.5 + 1):
+            return False
+        if (x[2] == y[2] and abs(float(x[1])-float(y[1])) != 1):
+            return False
+        else:
+            return True
+    return innerFunction
+```
+
+
+- Why is `x` (as in "`appointment`") this:
+```
+{'Day': "[['thu', 'sat', 'wed']]",
+ 'House': 'D',
+ 'Name': 'Logan',
+ 'Pref': "['Afternoon']",
+ 'Surname': 'Davies'}
+ ```
+ And when used in `constraintFunction()` it becomes this:
+ ```
+ ['sat', '11.00', 'D']
+```
+Are they different things?
