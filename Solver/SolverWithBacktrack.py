bddf48f2149ec330f2c0a602409d21129a17a5e8
Eleonora Miani <miani.eleonora@gmail.com>
2019-05-10 12:05:07 +0200

    Refactored SolverWithBacktrack.py

--- Solver/SolverWithBacktrack.py
+++ Solver/SolverWithBacktrack.py
@@ -12,56 +12,108 @@ from Backtracking_Appointments import backtrackingSearch
 current_milli_time = lambda: int(round(time.time() * 1000))
 
 
-
-
-"""Data per il dominio"""
-days = ["mon", "tue", "wed", "thu", "fri", "sat"]
-
-hours = ["08.00", "08.50", "09.00", "09.50", "10.00", "10.50", "11.00", "11.50",
-"13.00", "13.50", "14.00", "14.50", "15.00", "15.50", "16.00", "16.50", "17.00",
-"17.50"]
-
-locations = ["A", "B", "C", "D"]
-
-
-
 def takeSecond(elem):
     return elem[1]
 
 
-appointments = {}
-with open(sys.argv[1], 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        a = iter(row[1:])
-        appointments[row[0]] = dict(zip(a, a))
-print(appointments);
+# load appointments from a csv file in *filePath*
+def loadAppointments(filePath):
+    appointments = {}
 
-dominio = []
-count = 0
+    with open(filePath, 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            a = iter(row[1:])
+            appointments[row[0]] = dict(zip(a, a))
+
+    return appointments
 
 
-for i in days:
-    for y in hours:
-        for loc in locations:
-            dominio.append([i])
-            dominio[count].append(y)
-            dominio[count].append(loc)
-            count += 1
+# intialize a generic domain with all possible combinations of days, hours and locations
+def initDomain():
+    '''
+    Data per il dominio
+    '''
 
-#print(dominio)
+    days = ["mon", "tue", "wed", "thu", "fri", "sat"]
+
+    hours = ["08.00", "08.50", "09.00", "09.50", "10.00", "10.50", "11.00", "11.50",
+            "13.00", "13.50", "14.00", "14.50", "15.00", "15.50", "16.00", "16.50", "17.00",
+            "17.50"]
+
+    locations = ["A", "B", "C", "D"]
+
+    domain = []
+    count = 0
+
+    for i in days:
+        for y in hours:
+            for loc in locations:
+                domain.append([i])
+                domain[count].append(y)
+                domain[count].append(loc)
+                count += 1
+
+    return domain
+
+
+# print function to print a solution in a clean way
+def printSolution(solution):
+    days = ["mon", "tue", "wed", "thu", "fri", "sat"]
+
+    ordApp = [[], [], [], [], [], []]
+
+    for x in solution:
+        if solution[x][0] == days[0]:
+            ordApp[0].append([x, solution[x]])
+        if solution[x][0] == days[1]:
+            ordApp[1].append([x, solution[x]])
+        if solution[x][0] == days[2]:
+            ordApp[2].append([x, solution[x]])
+        if solution[x][0] == days[3]:
+            ordApp[3].append([x, solution[x]])
+        if solution[x][0] == days[4]:
+            ordApp[4].append([x, solution[x]])
+        if solution[x][0] == days[5]:
+            ordApp[5].append([x, solution[x]])
+
+    print(ordApp)
+    for x in ordApp:
+        x.sort(key=takeSecond)
+
+    index = 0
+    for x in ordApp:
+        print("\n\nGiorno: ", days[index])
+        print("\nMattina:")
+        cond = True
+        for y in x:
+            if (cond and float(y[1][1]) > 12):
+                print("\nPomeriggio:")
+                cond = False
+            print("Ore: ", y[1][1], "Casa: ", y[1][2], " Appuntamento con: ",
+                  appointments[y[0]]["Name"], " ", appointments[y[0]]["Surname"])
+        index += 1
+
+
+
+
+appointments = loadAppointments(sys.argv[1])
 #print(appointments)
+domain = initDomain()
+#print(domain)
 
-#problem = Problem()
+
 # Invece che problem faccio un grafo dei vincoli, aggiungo un nodo per ciascuna variabile e relativo domino.
 ConstraintGraph = nx.Graph()
 
 variablesName = []
-# sto iterando su tutte le chiavi
+
+# for each appointment (iterate on the numerical key of the appointments)
 for x in appointments:
     dom = []
 
-    for y in dominio:
+    # check which elements of the generic domain are necessary for this appointment
+    for y in domain:
         hour , minutes = y[1].split(".")
         hour = int(hour)
 
@@ -72,8 +124,7 @@ for x in appointments:
                 dom.append(y)
 
     #print(dom)
-    #problem.addVariable(x, dom)
-    #Aggiungo la variabile corrente con il dominio aggiutato
+    #Aggiungo la variabile corrente con il domain aggiustato
     ConstraintGraph.add_node(x, domain=dom)
     variablesName.append(x)
 
@@ -96,34 +147,4 @@ print("\n\n###########Time spent to find the first solution = ", end-start," ms.
 
 print(solution)
 
-ordApp = [[],[],[],[],[],[]]
-
-for x in solution:
-    if solution[x][0]==days[0]:
-        ordApp[0].append([x, solution[x]])
-    if solution[x][0]==days[1]:
-        ordApp[1].append([x, solution[x]])
-    if solution[x][0]==days[2]:
-        ordApp[2].append([x, solution[x]])
-    if solution[x][0]==days[3]:
-        ordApp[3].append([x, solution[x]])
-    if solution[x][0]==days[4]:
-        ordApp[4].append([x, solution[x]])
-    if solution[x][0]==days[5]:
-        ordApp[5].append([x, solution[x]])
-
-print(ordApp)
-for x in ordApp:
-    x.sort(key =  takeSecond)
-
-index = 0
-for x in ordApp:
-    print("\n\nGiorno: ", days[index])
-    print("\nMattina:")
-    cond = True
-    for y in x:
-        if (cond and float(y[1][1])>12):
-            print("\nPomeriggio:")
-            cond = False
-        print("Ore: ", y[1][1], "Casa: ", y[1][2], " Appuntamento con: ", appointments[y[0]]["Name"], " ", appointments[y[0]]["Surname"])
-    index+=1
+printSolution(solution)
